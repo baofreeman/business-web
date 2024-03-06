c953fc047ffe518bccc77f51d292eaa1185cb7a9
Hoang <freeman.dev10@gmail.com>
2024-03-07 03:44:17 +0700

    update

--- src/page/Cart.jsx
+++ src/page/Cart.jsx
@@ -124,7 +124,7 @@ const Cart = () => {
         </table>
       ) : (
         <div className="w-full flex flex-col justify-center items-center h-[100%]">
-          <span>Chưa có sản phẩm nào được chọn</span>
+          <span>Chưa có sản phẩm nàod được chọn</span>
           <Button size="m" design="link-primary" width="max" to={"/shop"}>
             Tiếp tục mua sắm
           </Button>
